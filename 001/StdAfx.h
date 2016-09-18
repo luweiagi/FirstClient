bf9cd8a23c4ba9484311b7a922f8705a48f45efe
luweikxy <luweikxy@163.com>
2016-09-19 02:33:35 +0800

    add project
    add c++ project

---
+++ 001/StdAfx.h
@@ -0,0 +1,22 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__D25B4A55_4C49_445B_AA1A_A2C0CDFAE605__INCLUDED_)
+#define AFX_STDAFX_H__D25B4A55_4C49_445B_AA1A_A2C0CDFAE605__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+
+#include <stdio.h>
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__D25B4A55_4C49_445B_AA1A_A2C0CDFAE605__INCLUDED_)
